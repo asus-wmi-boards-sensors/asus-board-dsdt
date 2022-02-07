233d3a7b697f650d3ac18c9f65218d85682b134a
Denis Pauk <pauk.denis@gmail.com>
2022-02-07 22:52:28 +0200

    Add Z490 aml

--- download_upack.sh
+++ download_upack.sh
@@ -17,6 +17,13 @@ for bios_file in \
     https://dlcdnets.asus.com/pub/ASUS/mb/BIOS/ROG-STRIX-Z390-F-GAMING-ASUS-2004.ZIP \
     https://dlcdnets.asus.com/pub/ASUS/mb/BIOS/ROG-STRIX-Z390-H-GAMING-ASUS-3006.ZIP \
     https://dlcdnets.asus.com/pub/ASUS/mb/BIOS/ROG-STRIX-Z390-I-GAMING-ASUS-3006.ZIP \
+    https://dlcdnets.asus.com/pub/ASUS/mb/BIOS/ROG-STRIX-Z490-A-GAMING-ASUS-2403.ZIP \
+    https://dlcdnets.asus.com/pub/ASUS/mb/BIOS/ROG-STRIX-Z490-E-GAMING-ASUS-2403.ZIP \
+    https://dlcdnets.asus.com/pub/ASUS/mb/BIOS/ROG-STRIX-Z490-F-GAMING-ASUS-2403.ZIP \
+    https://dlcdnets.asus.com/pub/ASUS/mb/BIOS/ROG-STRIX-Z490-G-GAMING-ASUS-2403.ZIP \
+    https://dlcdnets.asus.com/pub/ASUS/mb/BIOS/ROG-STRIX-Z490-G-GAMING-WIFI-ASUS-2403.ZIP \
+    https://dlcdnets.asus.com/pub/ASUS/mb/BIOS/ROG-STRIX-Z490-H-GAMING-ASUS-2403.ZIP \
+    https://dlcdnets.asus.com/pub/ASUS/mb/BIOS/ROG-STRIX-Z490-I-GAMING-ASUS-2403.ZIP \
     https://dlcdnets.asus.com/pub/ASUS/mb/BIOS/TUF-GAMING-X570-PLUS-ASUS-4021.ZIP \
     https://dlcdnets.asus.com/pub/ASUS/mb/LGA1150/MAXIMUS-VII-HERO/MAXIMUS-VII-HERO-ASUS-3201.zip # Use some different capsule format/ can get DSDT from it
 do
@@ -26,13 +33,13 @@ done
 # unpack
 ls -1 | grep -i "\.zip" |  awk '{print "unzip -o " $1 }' | sh -
 # remove old capsules and unrequired files
-rm -rf *.exe *CAP_output *.dsl *.aml || exit
+rm -rf *.exe *CAP_output *.dsl || exit
 # unpack capsules
 ls -1 *.CAP | awk '{print  "../bin/uefi-firmware-parser --brute -e -O " $1 }' | sh -  || exit
 # move all DSDL to root directory
-grep ALASKA * -R 2>&1 | sed 's|grep: ||g' | sed 's|: |\t|g' | awk '{print "file " $1}' | sh - | grep DSDT | sed 's|:|\t|g' | awk '{print "md5sum " $1}' | sh - | sed 's|/|\t|g' | awk '{print "cp -v " $2 "/" $3 "/" $4 "/" $5 "/" $6 "/" $7 "/" $8 "/" $9 " " $2 "." $1 ".aml" }' | sh - || exit
+grep ALASKA *CAP_output/* -R 2>&1 | sed 's|grep: ||g' | sed 's|: |\t|g' | awk '{print "file " $1}' | sh - | grep DSDT | sed 's|:|\t|g' | awk '{print "md5sum " $1}' | sh - | sed 's|/|\t|g' | awk '{print "cp -v " $2 "/" $3 "/" $4 "/" $5 "/" $6 "/" $7 "/" $8 "/" $9 " " $2 "." $1 ".aml" }' | sh - || exit
 # remove CAP_output from names
-ls -1 *.aml  | sed 's|\.|\t|g' | awk '{print "mv " $1 "." $2 "." $3 "." $4 " " $1 "." $3 "." $4}' | sh - || exit
+ls -1 *CAP_output*.aml  | sed 's|\.|\t|g' | awk '{print "mv " $1 "." $2 "." $3 "." $4 " " $1 "." $3 "." $4}' | sh - || exit
 # remove capsules unpacks
 rm -rf *CAP_output || exit
 # convert to dsl
