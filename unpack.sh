5999923b0f60989ff3df537fcb0e020f8b20a2aa
Denis Pauk <pauk.denis@gmail.com>
2022-11-12 23:52:14 +0200

    move dumps to board series

--- unpack.sh
+++ unpack.sh
@@ -3,7 +3,7 @@
 # unpack
 ls -1 | grep -i "\.zip" |  awk '{print "unzip -o " $1 }' | sh -
 # remove old capsules and unrequired files
-rm -rf *.exe *.EXE *CAP_output *.dsl || exit
+rm -rf *.exe *.EXE *CAP_output *.dsl */*.dsl || exit
 # unpack capsules
 ls -1 *.CAP | awk '{print  "../bin/uefi-firmware-parser --brute -e -O " $1 }' | sh -  || exit
 # move all DSDL to root directory
@@ -12,6 +12,30 @@ grep ALASKA *CAP_output/* -R 2>&1 | sed 's|grep: ||g' | sed 's|: |\t|g' | awk '{
 ls -1 *CAP_output*.aml  | sed 's|\.|\t|g' | awk '{print "mv " $1 "." $2 "." $3 "." $4 " " $1 "." $3 "." $4}' | sh - || exit
 # remove capsules unpacks
 rm -rf *CAP_output || exit
+rm -rf *.CAP || exit
+# create subdirs
+for dirname in \
+	"TUF-GAMING-B550" \
+	"TUF-GAMING-X570" \
+	"ROG-STRIX-B350" \
+	"ROG-STRIX-B450" \
+	"ROG-STRIX-B550" \
+	"ROG-STRIX-B650" \
+	"ROG-STRIX-X370" \
+	"ROG-STRIX-X470" \
+	"ROG-STRIX-X570" \
+	"ROG-STRIX-X670" \
+	"ROG-STRIX-Z370" \
+	"ROG-STRIX-Z390" \
+	"ROG-STRIX-Z490" \
+	"ROG-CROSSHAIR-VIII" \
+	"ROG-CROSSHAIR-X670" \
+	"PRIME-H410" \
+	"PRIME-X570"
+do
+	mkdir -p ${dirname}
+	mv ${dirname}*.aml ${dirname}
+done
 # convert to dsl
-ls -1 *.aml | awk '{print "iasl -d " $1}' | sh - || exit
+find * | grep "\.aml" | awk '{print "iasl -d " $1}' | sh - || exit
 
